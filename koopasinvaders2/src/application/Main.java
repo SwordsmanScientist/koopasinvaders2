c7451a723e3dc46f879aaebddbb074519201576f
SwordsmanScientist <36085359+SwordsmanScientist@users.noreply.github.com>
2018-04-26 17:52:24 -0500

    Add files via upload

---
+++ koopasinvaders2/src/application/Main.java
@@ -0,0 +1,37 @@
+package application;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.stage.Stage;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+
+
+/**start: Gets Start.fxml and sets the scene 
+ *with it.
+ *@param primaryStage
+ */
+public class Main extends Application {
+	public static Stage stage;
+	@Override
+	public void start(Stage primaryStage) {
+		try {
+			stage = primaryStage;
+			AnchorPane root = new AnchorPane();
+			root = FXMLLoader.load(getClass().getResource("/Start.fxml"));
+			Scene scene = new Scene(root);
+			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+			stage.setScene(scene);
+			stage.show();
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	/**main: launches the arguments.
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		launch(args);
+	}
+}
